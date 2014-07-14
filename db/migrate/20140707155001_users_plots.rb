b68aa2ea478df78d4726ee928c747d3ad078674e
gernest <geofreyernest@live.com>
2014-07-15 00:55:19 +0300

    removed id of the intermediate table

--- db/migrate/20140707155001_users_plots.rb
+++ db/migrate/20140707155001_users_plots.rb
@@ -1,6 +1,6 @@
 class UsersPlots < ActiveRecord::Migration
   def change
-      create_table :users_plots do |t|
+      create_table :users_plots, :id=>false do |t|
           t.integer :user_id
           t.integer :plot_id
       end
