4cedd09abd79f7763fe43cfaacf151f811ac372c
gernest <geofreyernest@live.com>
2014-07-16 00:48:37 +0300

    implemented make_a_note and make_a_post methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,22 @@ class User < ActiveRecord::Base
     has_and_belongs_to_many :plots
 
     def make_a_plan(title)
-        self.plans.create(:title=>title)
+        self.plans.create(:title => title)
+    end
+
+    def take_a_note(model, note)
+        if model.instance_of?(Plan) and self.id==model.user_id
+            self.plans.find_by_id(model.id).notes.create!(note)
+        elsif model.instance_of?(Note) and self.id==model.id
+            self.plans.find_by_id(model.plan_id).plots.notes.create!(note)
+        else
+            raise ArgumentError
+        end
+    end
+
+    def make_a_plot(plan, plot)
+        new_plot=self.plans.find_by_id(plan.id).plots.create! plot
+        self.plots<<new_plot
+        new_plot
     end
 end
