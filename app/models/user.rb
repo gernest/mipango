8aa7fbf1f5e4c0a7aa8e20fade585b39a6e88b52
gernest <geofreyernest@live.com>
2014-07-16 21:04:09 +0300

    implemented make_a_plan, make_a_plot and take a note methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,22 +9,40 @@ class User < ActiveRecord::Base
     has_and_belongs_to_many :plots
 
     def make_a_plan(title)
-        self.plans.create(:title => title)
+        self.plans.create!(:title => title)
     end
 
     def take_a_note(model, note)
-        if model.instance_of?(Plan) and self.id==model.user_id
-            self.plans.find_by_id(model.id).notes.create!(note)
-        elsif model.instance_of?(Note) and self.id==model.id
-            self.plans.find_by_id(model.plan_id).plots.notes.create!(note)
+        @note=note
+        @note[:user_id]=self.id
+
+        if model.instance_of?(Plan) and belongs_to_current_user(model)
+            model.notes.create!(@note)
+        elsif model.instance_of?(Plot)
+            #check if this user is one of the authors  and save
+            unless model.users.empty?
+                model.users.each do |usr|
+                    if usr.id==self.id
+                        return model.notes.create(@note)
+                    end
+                end
+            end
         else
             raise ArgumentError
         end
     end
 
     def make_a_plot(plan, plot)
-        new_plot=self.plans.find_by_id(plan.id).plots.create! plot
+        new_plot=self.plans.find_by_id(plan.id).plots.create(plot)
         self.plots<<new_plot
-        new_plot
+    end
+
+    private
+    def belongs_to_current_user(klass)
+        if klass.user_id==self.id
+            true
+        else
+            false
+        end
     end
 end
