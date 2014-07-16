f7eabf833f5af43274c1cc7ee0d18509726d8c00
gernest <geofreyernest@live.com>
2014-07-16 21:06:36 +0300

    added belongs to the same model

--- app/models/plot.rb
+++ app/models/plot.rb
@@ -2,6 +2,7 @@ class Plot < ActiveRecord::Base
     belongs_to :plan
     has_many :notes
     has_one :parent_plot, class_name: :Plot
+    belongs_to :parent_plot, :class_name => :Plot
     has_and_belongs_to_many :users
 
     validates :title, :presence => true
