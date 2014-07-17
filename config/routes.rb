ae4f55d982a438e477129bd2d4e43e724c54ad3c
gernest <geofreyernest@live.com>
2014-07-17 16:19:06 +0300

    rooting to "main#index" instead of "Batman#index"

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  get "(*redirect_path)", to: "batman#index", constraints: lambda { |request| request.format == "text/html" }
+  root to: "main#index"
+  get "(*redirect_path)", to: "main#index", constraints: lambda { |request| request.format == "text/html" }
 
-    devise_for :users
+  devise_for :users
 
-    resources :plans do
-        resources :plots do
-            resources :notes
-        end
-
-        resources :notes
+  resources :plans do
+    resources :plots do
+      resources :notes
     end
+
+    resources :notes
+  end
 end
