1341c77fb25c839cc8960d78b4f83c9b9a57015a
gernest <geofreyernest@live.com>
2014-07-18 13:22:58 +0300

    removed batman js

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  get "(*redirect_path)", to: "batman#index", constraints: lambda { |request| request.format == "text/html" }
-
-
   resources :plans do
     resources :plots do
       resources :notes
